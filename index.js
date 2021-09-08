1eac3bd225fa82d4218313535a2d89f2a5508789
Suzi Nogueira <asuzinogueira@gmail.com>
2021-09-07 22:49:04 -0300

    post, put e delete

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ app.get("/", (req,res)=>{
     res.send("<h1>Hello World from GET</h1>");
 })
 
+app.post("/",(req,res)=>{
+ 
+    res.send("<h1>Hello World from POST</h1>");
+})
+
+
+app.put("/",(req,res)=>{
+ 
+    res.send("<h1>Hello World from PUT</h1>");
+})
+
+app.delete("/",(req,res)=>{
+ 
+    res.send("<h1>Hello World from DELETE</h1>");
+})
+
+
 const PORT = 5000;
 app.listen(PORT, ()=> {
     console.log(`Server running on Port:${PORT}`);
